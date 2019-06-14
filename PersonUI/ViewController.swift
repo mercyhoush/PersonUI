7160e904daa79b804d530f1495241c88c8de0e20
Mercy Housh <mercyhoush@Mercys-MacBook-Pro.local>
2019-06-14 18:43:33 -0500

    first commit

---
+++ PersonUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonUI
+//
+//  Created by Mercy Housh on 6/14/19.
+//  Copyright © 2019 Mercy Housh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
