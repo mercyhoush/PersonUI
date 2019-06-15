8d9007cdc58a7ee83e30ee49414babf3c720247d
Mercy Housh <mercyhoush@Mercys-MacBook-Pro.local>
2019-06-15 11:12:43 -0500

    built UI

--- PersonUI/ViewController.swift
+++ PersonUI/ViewController.swift
@@ -14,7 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 }
 
